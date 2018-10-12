7af211889bb2d64f2322f0743b1b04979e6f4061
Daltimore <obinnaleo78@gmail.com>
2018-10-12 21:07:52 +0100

    fix: index page

--- private/shared/index_html_header.php
+++ private/shared/index_html_header.php
@@ -12,7 +12,7 @@
 
     <!-- Bootstrap core CSS -->
     <link href="./posts/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.4.1/css/all.css" integrity="sha384-5sAR7xN1Nv6T6+dT2mhtzEpVJvfS3NScPQTrOxhwjIuvcA67KV2R5Jz6kr4abQsz" crossorigin="anonymous">
     <link href="./posts/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
     <link href='https://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
     <link href='https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
